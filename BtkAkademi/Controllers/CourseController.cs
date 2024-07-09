c5be5711d6d025854bdee4835eacdf4f32e0293d
Hamza <kiziltoprakh55@gmail.com>
2024-07-09 11:00:37 +0300

    initial

---
+++ BtkAkademi/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BtkAkademi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BtkAkademi.Controllers
+{
+
+    public class CourseController : Controller
+    {
+       public IActionResult Index()
+       {
+            var model = Repository.Applications;
+        
+           return View("Index",model);
+       }
+       // get için kullanılır
+       public IActionResult Apply()
+       {
+        
+           return View();
+       }
+
+         // post için kullanılır
+         [HttpPost] // formdan gelen veriyi almak için
+         [ValidateAntiForgeryToken] // hangi tarayıcıyla çalışıyorsa onunla çalışır ve güvenlik sağlar
+       public IActionResult Apply([FromForm] Candidate model)
+       {
+            Repository.Add(model);
+
+           return View("Feedback",model);
+       }
+    
+    }
+}
+
---
+++ BtkAkademi/Models/Candidate.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;    
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
