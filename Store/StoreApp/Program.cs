6b4579100e9f6e04d1e59f76b4a2b5dc7328dcff
Hamza <kiziltoprakh55@gmail.com>
2024-07-10 14:36:48 +0300

    _migrations

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<RepositoryContext>(options => 
+    {
+        options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+    });
 var app = builder.Build();
 
 app.UseHttpsRedirection();
