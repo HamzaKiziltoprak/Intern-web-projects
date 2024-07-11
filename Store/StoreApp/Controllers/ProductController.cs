23ba590da14cc0d76e694d1dd1b3bdd001c18860
Hamza <kiziltoprakh55@gmail.com>
2024-07-11 11:29:38 +0300

    fixed _dependency ijection
    The error emerging in ProductController class' class inheritance section;  ControllerBase => Controller.

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -12,7 +12,7 @@ namespace StoreApp.Controllers
 {
     // [ApiController]
     // [Route("api/[controller]")] // api/product ne işe yarar : api/product ile başlayan istekler bu controller'a yönlendirilir.
-    public class ProductController : ControllerBase
+    public class ProductController : Controller
     {
         private readonly RepositoryContext _context;
         //*************************************************************
@@ -22,25 +22,27 @@ namespace StoreApp.Controllers
             _context = context;
         }
 
+        // alternative
+        // public IEnumerable<Product> Index()
+        // {
+        //     // var context = new RepositoryContext(
+        //     //     new DbContextOptionsBuilder<RepositoryContext>()
+        //     //     .UseSqlite("Data Source = C:\\sqlite3\\ProductDb.db")
+        //     //     .Options);
 
-        public IEnumerable<Product> Index()
+        //     return _context.Products;
+        // }
+
+
+
+        public IActionResult Index()
         {
-            // var context = new RepositoryContext(
-            //     new DbContextOptionsBuilder<RepositoryContext>()
-            //     .UseSqlite("Data Source = C:\\sqlite3\\ProductDb.db")
-            //     .Options);
-
-            return _context.Products;
+            var model = _context.Products.ToList();
+            return View(model);
         }
 
 
-        // düzeltemedim hala 
-        // -**************************-
-        // public IActionResult Index()
-        // {
-        //     var model = _context.Products.ToList();
-        //     return View(model);
-        // }
+
 
 
     }
