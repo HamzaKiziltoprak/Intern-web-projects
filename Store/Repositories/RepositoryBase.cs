1d50c8e61b9458382bf9b71a090108b6d3002f67
Hamza <kiziltoprakh55@gmail.com>
2024-07-16 12:03:24 +0300

    _RepositoryBase

---
+++ Store/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class, new()
+    {
+        protected readonly RepositoryContext _context;
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return trackChanges
+                ? _context.Set<T>()
+                : _context.Set<T>().AsNoTracking();  // AsNoTracking() methodu, veritabanından veri çekerken, verilerin takip edilmesini istemediğimizde kullanılır.
+        }
+    }
+}
