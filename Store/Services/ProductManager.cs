98fbee6f40c6161da4e9da114553b5158d9dade8
Hamza <kiziltoprakh55@gmail.com>
2024-07-17 19:04:32 +0300

    Services , _Product and Category services
    ve add new layer in project , classlib Service project , then create  interface and concrete class of Category and  Product.

---
+++ Store/Services/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IRepositoryManager _manager;
+
+        public ProductManager(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+        public Product? GetOneProduct(int id, bool trackChanges)
+        {
+            //return _manager.Product.GetAllProducts(trackChanges);
+            var product = _manager.Product.GetOneProduct(id, trackChanges);
+            if (product is null)
+                throw new Exception("Product Not found!");
+            return product;
+        }
+
+        public IEnumerable<Product> GetAllProducts(bool trackChanges)
+        {
+            return _manager.Product.GetAllProducts(trackChanges);
+        }
+    }
+}
