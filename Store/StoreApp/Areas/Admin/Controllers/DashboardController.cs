dcf56fa1438a306b3a7b24479300ade3dbb5d48a
Hamza <kiziltoprakh55@gmail.com>
2024-07-21 01:07:15 +0300

    _Areas , _Layout , _Navbar
    I fix dynamic menu bar for homepage and clone it for admin page. then ı made controolers to acces products,categories and Dashboard page

---
+++ Store/StoreApp/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Areas.Admin.Controllers
+{
+    [Area("Admin")] // This is the area name which is used to identify the controllers in the Admin area
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
